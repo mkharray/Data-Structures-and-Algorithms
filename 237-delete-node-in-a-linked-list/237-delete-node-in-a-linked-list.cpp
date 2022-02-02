781251637d11382d9a6bff39ab6fe386c25088be
Mayank Khare <mayank.209301260@muj.manipal.edu>
2022-02-02 12:33:48 +0530

    Time: 12 ms (84.52%), Space: 7.7 MB (38.17%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
@@ -12,5 +12,9 @@ public:
         node->val = node->next->val;
         node->next = node->next->next;
         
+        
+        /* e.g- 4->5->9      todelnode = 5
+         so first we change the value of todelnode to its next
+         then we simply change todelnext's next(pointer) to next's next*/
     }
 };
