aeed9df6f3599992fdf9ce7bf03f602cf71bc8f1
Mayank Khare <mayank.209301260@muj.manipal.edu>
2022-07-04 23:46:48 +0530

    Time: 237 ms (23.13%), Space: 93.3 MB (52.18%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        //brute force solution          O(n*n)
+        int profit = 0;
+        int mini = prices[0];
+        for(int i = 1;i<prices.size(); i++){
+        
+        profit = max(profit,prices[i] - mini);
+
+        if(mini > prices[i])
+            mini = prices[i];
+        
+        }
+        return profit;
+    }
+};
