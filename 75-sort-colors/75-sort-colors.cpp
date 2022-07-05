755bc33ba7a34f062b4bbf02e3b4b66d30e6b000
Mayank Khare <mayank.209301260@muj.manipal.edu>
2022-07-05 23:19:00 +0530

    Time: 4 ms (44.17%), Space: 8.2 MB (90.44%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        vector<int>v (3,0);
+        vector <int> v {0,0,0};
         for(auto i : nums)
-            v[i]++;
-
+           v[i]++;
         nums.clear();
-            for(int i = 0; i < 3; i++){
-                while(v[i] > 0){
-                    nums.push_back(i);
-                    v[i]--;
-                }
+        for(int i = 0; i < 3; i++){
+            while(v[i] > 0)
+            {
+            nums.push_back(i);
+            v[i]--;
             }
-            
+        }
     }
 };
