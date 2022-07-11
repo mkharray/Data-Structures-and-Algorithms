00ad84fe48808fee68af98c22a36ac3b96a88325
Mayank Khare <mayank.209301260@muj.manipal.edu>
2022-07-12 00:43:12 +0530

    Time: 35 ms (62.70%), Space: 25 MB (37.11%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+    int product = 1;
+        int countzero = 0;
+    unordered_set<int>us;
+        for(int  i = 0; i < nums.size(); i++){
+        if(nums[i] == 0){
+            us.insert({i});
+            countzero++;
+        }
+            else
+            product *=nums[i];
+        }
+        
+        vector<int>ans;
+        for(int  i = 0; i < nums.size(); i++){
+        if(nums[i] == 0 && countzero == 1)
+            ans.push_back(product);
+        else if( countzero > 0)
+            ans.push_back(0);
+            else
+                ans.push_back(product/nums[i]);
+        }
+        
+        return ans;
+    }
+};
