784f3912397fe46c4685fc9ee9038c72f5fba7ff
Mayank Khare <mayank.209301260@muj.manipal.edu>
2022-07-13 00:05:41 +0530

    Time: 183 ms (5.02%), Space: 94.3 MB (5.01%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.cpp
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     string decodeMessage(string key, string message) {
-        char curr = 'a';
-        unordered_map<char,char>m;
+     unordered_map<char,char>m;
+        char ch = 'a';
         for(int i = 0; i < key.length(); i++){
-         if(m.find(key[i]) == m.end() && key[i] != ' '){
-             m.insert({key[i] , curr});
-             curr++;
-         }
-
+        if(m.find(key[i]) == m.end() && key[i] != ' '){
+            m.insert({key[i], ch});
+            ch++;
         }
-        string ans="";
+            cout<<key[i]<< ' '<<ch<<endl;
+        }
+      
+        string ans = "";
         for(int i = 0; i < message.length(); i++){
-            if(message[i] == ' ')
-                ans = ans + ' ';
+      if(message[i] == ' ')
+          ans = ans + ' ';
             else
-            ans = ans + m[message[i]];
-            
-        }
-        return ans;
+                ans = ans+m[message[i]];
     }
+    return ans;}
 };
