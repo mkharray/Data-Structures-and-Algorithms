fca23a023b5694be670b059b27436c1ff8df5e74
Mayank Khare <mayank.209301260@muj.manipal.edu>
2022-07-17 00:27:38 +0530

    Time: 179 ms (18.96%), Space: 16.6 MB (75.37%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -7,32 +7,44 @@ public:
         if(s == t)
             return 0;
         
+        vector<int>v(26,0);
+        for(int i = 0; i < s.length(); i++){
+            v[s[i] - 'a']++;
+            v[t[i] - 'a']--;
+        }
         int steps = 0;
-        unordered_map<char,int>m;
-        unordered_map<char,int>n;
+        for(int i = 0; i < 26; i++)
+            if(v[i] > 0)
+                steps+=v[i];
         
-        for(int i = 0; i < s.length(); i++){
-        if(m.find(s[i]) == m.end() )
-            m.insert({s[i],1});
-        else
-            m[s[i]]++;
-        }
-        
-        for(int i = 0; i < s.length(); i++){
-        if(n.find(t[i]) == n.end() )
-            n.insert({t[i],1});
-        else
-            n[t[i]]++;
-        }
-        char x;
-        for(int i = 0; i < s.length(); i++){
-            
-            if (m[s[i]] > n[s[i]] && s[i] != x ) {
-                steps += (m[s[i]] - n[s[i]]);
-            cout<<s[i]<<' ' << m[s[i]] <<  " " <<n[s[i]]<<endl;
-            }
-               x = s[i]; 
-        }
         return steps;
+        
+//         int steps = 0;
+//         unordered_map<char,int>m;
+//         unordered_map<char,int>n;
+        
+//         for(int i = 0; i < s.length(); i++){
+//         if(m.find(s[i]) == m.end() )
+//             m.insert({s[i],1});
+//         else
+//             m[s[i]]++;
+//         }
+        
+//         for(int i = 0; i < s.length(); i++){
+//         if(n.find(t[i]) == n.end() )
+//             n.insert({t[i],1});
+//         else
+//             n[t[i]]++;
+//         }
+//         char x;
+//         for(int i = 0; i < s.length(); i++){
+            
+//             if (m[s[i]] > n[s[i]] && s[i] != x ) {
+//                 steps += (m[s[i]] - n[s[i]]);
+//             cout<<s[i]<<' ' << m[s[i]] <<  " " <<n[s[i]]<<endl;
+//             }
+//                x = s[i]; 
+//         }
+//         return steps;
     }
 };
