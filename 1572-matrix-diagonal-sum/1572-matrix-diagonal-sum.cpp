2c0ab3b4564fbc2e944f249942bd5edf86fa06e2
Mayank Khare <mayank.209301260@muj.manipal.edu>
2022-07-28 23:09:48 +0530

    Time: 22 ms (54.94%), Space: 11.1 MB (99.83%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -2,12 +2,22 @@ class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
         int ans = 0;
-        for(int i = 0; i < mat.size(); i++){
-            for(int j = 0; j < mat.size(); j++){
-                if(i == j || (i+j) == mat.size()-1)
-                    ans+=mat[i][j];
-            }
-        }
+        for(int i = 0; i < mat.size(); i++)
+            ans+= mat[i][i]+mat[i][mat.size()-i-1];  // (i+j-1) in other form
+        
+        if( mat.size() % 2 != 0)
+        ans-= mat[(int)mat.size()/2][(int)mat.size()/2];
         return ans;
+        
+        
+        // O(n^2) Soln.
+        //int ans = 0;
+        // for(int i = 0; i < mat.size(); i++){
+        //     for(int j = 0; j < mat.size(); j++){
+        //         if(i == j || (i+j) == mat.size()-1)
+        //             ans+=mat[i][j];
+        //     }
+        // }
+        // return ans;
     }
 };
