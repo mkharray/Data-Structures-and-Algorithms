440187dde80380ee066e0abfded4a7d4aff2fbb2
Mayank Khare <mayank.209301260@muj.manipal.edu>
2022-07-29 14:50:55 +0530

    Time: 11 ms (19.71%), Space: 9.6 MB (51.72%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int x;
+        
+        int index = matrix[0].size()-1;
+        int i = 0;
+            
+            while(index >=0 && i < matrix.size()){
+                 x = matrix[i][index];
+                if(target== x){
+                 cout<<x<<" "<< (matrix[i][index]);
+                    return true;
+                }
+                else if(target < x)
+                    index--;
+                else 
+                    i++;
+                }
+
+    return false;    
+    }
+};
