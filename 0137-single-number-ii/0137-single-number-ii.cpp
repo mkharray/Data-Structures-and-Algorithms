747b9ff67ee0c5cc1f66ced656d4ad9640665ab5
Mayank Khare <mayank.209301260@muj.manipal.edu>
2023-06-07 16:37:44 +0530

    Time: 4 ms (91.06%), Space: 9.5 MB (48.69%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         
-        if(nums.size() == 1 || nums.size() == 2)
+        if(nums.size() == 1)
             return nums[0];
         for(int i = 0; i < nums.size() - 2;){
             if(nums[i] == nums[i+1] && nums[i+1] == nums[i+2])
