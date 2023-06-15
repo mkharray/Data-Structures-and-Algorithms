ca62ddbd6d4f4b7c99afba9f007c637d2f004bae
Mayank Khare <mayank.209301260@muj.manipal.edu>
2023-06-16 01:07:00 +0530

    Time: 4 ms (36.18%), Space: 6.1 MB (46.41%) - LeetHub

---
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int c = 0 ;
+        for(int i = 0; i < s.length();i++){
+            if(s[i] == letter)
+                c++;
+
+        }
+        
+        return(c*100)/s.length();
+    }
+};
