5b7fab55d1698915a2160dccf72df4e3bb851176
Mayank Khare <mayank.209301260@muj.manipal.edu>
2023-06-16 22:51:30 +0530

    Time: 159 ms (5.04%), Space: 295.6 MB (5.01%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string revIt(string x){
+        string ans = "";
+        for(char ch : x)
+            ans = ch+ans;
+        return ans;
+    }
+    string reverseWords(string s) {
+        s = s+' ';
+        string ans = "";
+        int x = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == ' '){
+                ans = ans + revIt(s.substr(x,i-x))+' ';
+                x = i+1;
+                
+                }
+        }
+        return ans.substr(0,ans.length()-1);
+    }
+};
