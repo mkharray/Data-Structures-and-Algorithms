0b755a7d0c32454c0b4ef1220bef16634f7df393
Mayank Khare <mayank.209301260@muj.manipal.edu>
2023-06-29 14:07:34 +0530

    Time: 1135 ms (23.19%), Space: 41.2 MB (73.49%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int findgreater(ListNode* node){
+        int nodeval = node->val;
+        node = node->next;
+
+        while(node != NULL){
+            if(node->val > nodeval)
+                return node->val;
+            node = node->next;
+
+        }
+        return 0;
+    }
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int>ans;
+        
+        while(head != NULL){
+            ans.push_back(findgreater(head));
+            head = head->next;
+        }
+        return ans;
+    }
+};
