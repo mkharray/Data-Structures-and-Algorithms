20501a09a7bfe26a625276fd949b322f7c5662bd
Mayank Khare <mayank.209301260@muj.manipal.edu>
2023-07-15 16:35:35 +0530

    Time: 82 ms (34.76%), Space: 48.3 MB (95.49%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size()-1;
+        int lastgoodindex = n;
+    for(int i = n-1; i >= 0; i--){
+            if((nums[i]+i) >= lastgoodindex)
+                lastgoodindex = i;
+            else if(nums[i] == 0){}
+    }
+        return lastgoodindex == 0;
+    }
+};
