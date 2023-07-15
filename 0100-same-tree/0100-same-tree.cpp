f703e58e69b32ab883c4b5c62cf0c9b8fc9bfc74
Mayank Khare <mayank.209301260@muj.manipal.edu>
2023-07-15 15:56:31 +0530

    Time: 3 ms (52.89%), Space: 10 MB (56.66%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,16 +12,12 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == NULL && q != NULL)
-            return false;
-        if(p == NULL && q == NULL)
+         if (p == nullptr && q == nullptr)
             return true;
-        if(p != NULL && q == NULL)
+        if (p == nullptr || q == nullptr)
             return false;
-        
-        if((p->val == q->val) && (isSameTree(p->left, q->left) == true ) && (isSameTree(p->right, q->right)) == true)
-            return true;
-        else
+        if (p->val != q->val)
             return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
