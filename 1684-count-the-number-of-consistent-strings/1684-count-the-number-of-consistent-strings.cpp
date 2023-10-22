5c76e2ea300a574d2c57fb532fc119d2157113d9
Mayank Khare <mayank.209301260@muj.manipal.edu>
2023-10-23 00:35:54 +0530

    Time: 39 ms (96.76%), Space: 30.4 MB (30.11%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -6,11 +6,9 @@ public:
             s.insert(ch);
         }
         int flag;
-        string st;
         int c = 0;
-        for(int j = 0; j < words.size(); j++ ){
+        for(string st : words ){
             flag = 1;
-            st = words[j];
             for(int i = 0; i < st.size();i++ ){
               if(s.find(st[i]) == s.end() )
               {
