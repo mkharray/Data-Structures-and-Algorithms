824b95a8c4db39475c2389cd90fb95d3c7d36ec1
Mayank Khare <mayank.209301260@muj.manipal.edu>
2024-03-16 23:28:05 +0530

    Time: 4 ms (36.70%) | Memory: 14.9 MB (14.29%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -11,20 +11,31 @@
  */
 class Solution {
 public:
-    void solve(int &sum, TreeNode* root){
-        if(!root)
-        return;
-        
-        if(root->left && !root->left->left && !root->left->right)
-        sum+=root->left->val;
-
-        solve(sum,root->left);
-        solve(sum, root->right);
-
-    }
     int sumOfLeftLeaves(TreeNode* root) {
+        queue<TreeNode*>q;
+        q.push(root);
         int sum = 0;
-        solve(sum,root);
+        while(!q.empty() )
+        {
+            int sz = q.size();
+
+            for(int i = 0; i < sz; i++){
+                TreeNode* curr = q.front();
+                q.pop();
+
+                if(!curr)
+                break;
+
+                if(curr->left){
+                    q.push(curr->left);
+                    TreeNode* left = curr->left;
+                    if(!left->left && !left->right)
+                    sum+=left->val;
+                }
+                if(curr->right)
+                q.push(curr->right);
+            }
+        }
         return sum;
     }
 };
