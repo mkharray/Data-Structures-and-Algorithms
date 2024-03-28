aa4fcfe9e576824de4f12dcd914527085b92fd48
Mayank Khare <mayank.209301260@muj.manipal.edu>
2024-03-28 19:48:26 +0530

    Time: 47 ms (32.06%) | Memory: 58.7 MB (15.33%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        
         int i = 0;
         int j = 0;
         int notOne = 0;
         int ans = 0;
-        queue<int>notOneLoc;
 
-        while(j < nums.size() ){
-            if(nums[j] != 1){
-            notOne++;
-            notOneLoc.push(j);
+        while (j < nums.size()) {
+            if (nums[j] != 1) {
+                notOne++;
             }
 
-            if(notOne > 1){
-            i = notOneLoc.front()+1;
-            notOne--;
-            notOneLoc.pop();
+            while (notOne > 1) {
+                if (nums[i] != 1) {
+                    notOne--;
+                }
+                i++;
             }
 
-            ans = max(ans, (j-i));
+            ans = max(ans, j - i);
             j++;
         }
+
         return ans;
     }
 };
