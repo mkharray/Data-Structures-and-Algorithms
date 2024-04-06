721f9a4bd011145ea43e3634a9c539db2e681602
Mayank Khare <mayank.209301260@muj.manipal.edu>
2024-04-07 00:09:05 +0530

    Time: 24 ms (37.61%) | Memory: 13 MB (46.42%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        stack<int> st;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '(') {
-                st.push(i);
-            } else if (s[i] == ')') {
-                if (!st.empty() && s[st.top()] == '(') {
-                    st.pop();
-                } else {
-                    s[i] = '*'; // Mark invalid closing parentheses
-                }
+        stack<int>st;
+        string ans = "";
+
+        for(int i = 0; i < s.size();i++){
+            if(s[i] == '(')
+            st.push(i);
+            else if(s[i] == ')'){
+                if(st.empty() == true)
+                s[i] = '#';
+                else
+                st.pop();
             }
         }
-        
-        // Mark invalid opening parentheses
-        while (!st.empty()) {
-            s[st.top()] = '*';
+
+        while(st.empty() != true){
+            s[st.top()] = '#';
             st.pop();
         }
-        
-        // Remove marked characters
-        string ans;
-        for (char c : s) {
-            if (c != '*') {
-                ans.push_back(c);
-            }
+
+        for(char c : s){
+            if(c != '#')
+            ans+=c;
         }
-        
         return ans;
     }
 };
