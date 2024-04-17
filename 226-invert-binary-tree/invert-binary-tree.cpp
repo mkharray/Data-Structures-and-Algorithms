16f41210ceef30537df2a1083458f0e63f9cf04e
Mayank Khare <mayank.209301260@muj.manipal.edu>
2024-04-17 13:01:48 +0530

    Time: 2 ms (45.09%) | Memory: 11.4 MB (51.66%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -11,19 +11,23 @@
  */
 class Solution {
 public:
-    void invert(TreeNode *root){
-        if(root == NULL)
+    void invert(TreeNode* root){
+        if(root == NULL )
         return;
+
+        TreeNode* node = new TreeNode();
+        node = root->left;
+
+        root->left = root->right;
+        root->right = node;
 
         invert(root->left);
         invert(root->right);
 
-        TreeNode* temp = root->left;
-        root->left=root->right;
-        root->right = temp; 
     }
     TreeNode* invertTree(TreeNode* root) {
         invert(root);
         return root;
+        
     }
 };
